f5204ccc2132c841ad4c38e2ad2a0e7ba21dd56d
pic <piccinini@gmail.com>
2011-03-09 17:10:59 +0100

    author info

--- init.rb
+++ init.rb
@@ -11,4 +11,7 @@ Redmine::Plugin.register :redmine_status_notifications do
   name 'Redmine Status Notifications plugin'
   description 'This plugin send extra notifications on status update'
   version '0.0.1'
+  url 'http://github.com/pic/redmine_status_notifications'
+  author 'Nicola Piccinini'
+  author_url 'mailto:piccinini@gmail.com'
 end
